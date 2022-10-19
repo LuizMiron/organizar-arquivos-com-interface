57ea0f958187f6e7ccaf2db3ac723f10b1aa91f4
LuizMiron <luiz.gugamiron@gmail.com>
2022-10-18 22:41:39 -0300

    atualizações

--- app.py
+++ app.py
@@ -18,6 +18,8 @@ def obter_destino():
 janela = tkinter.Tk()
 janela.geometry("500x600")
 janela.title("main")
+titulo = tkinter.Label(janela, text="Organizar arquivos")
+titulo.pack(side=tkinter.TOP)
 diretorios = tkinter.Frame(janela)
 diretorios.pack()
 cOrigem = tkinter.Frame(diretorios)
@@ -27,11 +29,11 @@ cDestino.pack(side=tkinter.BOTTOM)
 
 #2.1 ADICIONANDO A ORIGEM
 # label com explicando o funcionamento
-texto = tkinter.Label(cOrigem, text="adicione a origem")
-texto.pack(side=tkinter.TOP)
+texto = tkinter.Label(cOrigem, text="adicione a origem:")
+texto.pack(side=tkinter.LEFT)
 
 #input de origem
-vOrigem = tkinter.Entry(cOrigem, width=60)
+vOrigem = tkinter.Entry(cOrigem, width=40)
 vOrigem.pack(side=tkinter.LEFT)
 
 #botão de origem
@@ -41,10 +43,10 @@ btnOrigem.pack(side=tkinter.RIGHT)
 
 #2.2 ADICIONANDO O DESTINO
 # label com explicando o funcionamento
-texto = tkinter.Label(cDestino, text="adicione o destino")
-texto.pack(side=tkinter.TOP)
+texto = tkinter.Label(cDestino, text="adicione o destino:")
+texto.pack(side=tkinter.LEFT)
 # input de destino
-vDestino = tkinter.Entry(cDestino, width=60)
+vDestino = tkinter.Entry(cDestino, width=40)
 vDestino.pack(side=tkinter.LEFT)
 # botão de origem
 btnOrigem = tkinter.Button(cDestino, text="procurar", command=obter_destino)
